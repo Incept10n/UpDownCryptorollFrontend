def2b2dcbb4fea1e60621d1efa0415d0af5f17c2
max <m.d.mihailik@gmail.com>
2024-11-17 17:39:59 +0300

    fix: z index of profile menu

--- src/components/profile/profileComponents/ProfileMenu.tsx
+++ src/components/profile/profileComponents/ProfileMenu.tsx
@@ -8,7 +8,7 @@ const ProfileMenu = () => {
 
     return (
         <div
-            className="w-[295px] h-auto UpDownDarkBlueBg lg:block hidden
+            className="w-[295px] h-auto UpDownDarkBlueBg lg:block hidden z-20
                        border-[#999999] border-opacity-[70%] border-[1px] rounded-[35px] p-[35px]"
         >
             <div className="text-[#ccc3c3] text-opacity-[50%] text-[24px] font-light">
