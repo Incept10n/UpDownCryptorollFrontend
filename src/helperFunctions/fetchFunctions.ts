d35c9675bcf25a3453d59886e5a9c6e395a18659
Incept1on <incept1onnn999@gmail.com>
2024-12-23 12:07:33 +0300

    add console log for url

--- src/helperFunctions/fetchFunctions.ts
+++ src/helperFunctions/fetchFunctions.ts
@@ -170,7 +170,7 @@ export const fetchCurrentUserMatch = async (username: string) => {
 export const fetchCurrentPrice: (coin: GameCoice) => Promise<number> = async (
     coin: GameCoice,
 ) => {
-    console.log("l;kdsafj;lkasfdj;alksdfjlsa;kdfjalksfdjlkasdfjlkdsjflkdsjflksdjflksdjflkdsfjlkdsfjlksdjflksdjf");
+    console.log(`THE BACK URL IS ${backendUrl}`);
     const response = await fetch(
         `${backendUrl}/livePrice?sym=${Converter.getCoinFromGameChoice(coin)}`,
     );
